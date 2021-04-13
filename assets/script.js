98aeffc4b00f595c3e4b95cac44afecada611953
Peter Bruckner <peterbruckner08@gmail.com>
2021-04-13 22:27:34 +0930

    finished assisgment

--- assets/script.js
+++ assets/script.js
@@ -10,7 +10,7 @@ const searchWeather = (event) => {
   const searchTerm = getSearchTerm();
   const requestUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${searchTerm}&appid=${apiKey}&units=metric`;
 
- 
+
 
   fetch(requestUrl)
   .then(function (response) {
@@ -26,34 +26,35 @@ const searchWeather = (event) => {
     
     //day1
     document.querySelector('#date1').innerText = data.list[10].dt_txt
-    document.querySelector('#emoji1').innerText = data.list[10].weather[0].icon
+    document.querySelector('#emoji1').setAttribute('src', `http://openweathermap.org/img/wn/${data.list[10].weather[0].icon}@2x.png`);
     document.querySelector('#maxTemp1').innerText = "Max Temp: " + data.list[10].main.temp_max + "°"
     document.querySelector('#humidity1').innerText = "Humidity: " + data.list[10].main.humidity + '%'
     
     //day2
     document.querySelector('#date2').innerText = data.list[18].dt_txt
-    document.querySelector('#emoji2').innerText = data.list[18].weather[0].icon
+    document.querySelector('#emoji2').setAttribute('src', `http://openweathermap.org/img/wn/${data.list[18].weather[0].icon}@2x.png`);
     document.querySelector('#maxTemp2').innerText = "Max Temp: " + data.list[18].main.temp_max + "°"
     document.querySelector('#humidity2').innerText = "Humidity: " + data.list[18].main.humidity + '%'
     
     //day3
     document.querySelector('#date3').innerText = data.list[26].dt_txt
-    document.querySelector('#emoji3').innerText = data.list[26].weather[0].icon
+    document.querySelector('#emoji3').setAttribute('src', `http://openweathermap.org/img/wn/${data.list[26].weather[0].icon}@2x.png`);
     document.querySelector('#maxTemp3').innerText = "Max Temp: " + data.list[26].main.temp_max + "°"
     document.querySelector('#humidity3').innerText = "Humidity: " + data.list[26].main.humidity + '%'
     
     //day4
     document.querySelector('#date4').innerText = data.list[34].dt_txt
-    document.querySelector('#emoji4').innerText = data.list[34].weather[0].icon
+    document.querySelector('#emoji4').setAttribute('src', `http://openweathermap.org/img/wn/${data.list[34].weather[0].icon}@2x.png`);
     document.querySelector('#maxTemp4').innerText = "Max Temp: " + data.list[34].main.temp_max + "°"
     document.querySelector('#humidity4').innerText = "Humidity: " + data.list[34].main.humidity + '%' 
   });
 
 }
 
+
 searchFormEl.addEventListener('submit', searchWeather);
   
-localStorage.setItem(searchWeather)
+
 
 //citiesString = JSONstringify(cities)
 //localStorage.setItem("cities", [citiesString])
